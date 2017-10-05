6741ec5c379b3a48375bc31ebb2a884d4d1f3e9e
Francismar Condor <trollzer64@gmail.com>
2017-10-05 08:04:00 -0300

    Modularização extra e pronto para entrega

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 typedef enum { false, true } bool;
 
 void menu ();
-void jogo_base (int modo);
+void jogo_base (unsigned short int modo);
+void entrada (unsigned short int nivel, unsigned short int resposta[], unsigned short int i, bool turno);
 void main () {
 
 	unsigned short int op;
@@ -38,21 +39,17 @@ void menu () {
 	printf("(3) Sair\n");
 	printf("\nSua escolha: ");
 }
-void jogo_base (int modo) {
+void jogo_base (unsigned short int modo) {
 
 	int i;
-	unsigned short int proposta[50], resposta[50], sequencia = 1, nivel = 1, cont = 0;
+	unsigned short int proposta[50], resposta[50], sequencia = 1, nivel = 1;
 	bool turno = true, erro = false;
 
 	srand(time(NULL));
 	system("cls");
 
 	while (erro == false) {
-		for (i = cont; i < sequencia; i++) {
-			proposta[i] = 1 + rand()%(nivel*3);
-		}
-		cont++;
-
+		proposta[sequencia-1] = 1 + rand()%(nivel*3);
 		for (i = 0; i < sequencia; i++) {
 			printf("COMPUTADOR\nN%cvel %d\n", 161, nivel);
 			sleep(1);
@@ -65,23 +62,18 @@ void jogo_base (int modo) {
 		if (turno == true) {
 			i=0;
 			while ((erro == false)&&(i < sequencia)){
-				printf("JOGADOR 1\nN%cvel %d\n", 161, nivel);
-
-				scanf("%hu", &resposta[i]);
-				setbuf(stdin, NULL);
-
-				system("cls");
+				entrada(nivel, resposta, i, turno);
 
 				if (proposta[i] != resposta[i]) {
-					printf("RESPOSTA INCORRETA\n");
+						printf("RESPOSTA INCORRETA\n");
+						if (modo == 2) {
+							printf("Jogador 2 venceu\n");
+						} else {
+							printf("Perdeu no N%cvel %d\n", 161, nivel);
+						}
 					erro = true;
-
-					if (modo == 2) {
-						printf("Jogador 2 venceu\n");
-					} else {
-						printf("Perdeu no N%cvel %d\n", 161, nivel);
-					}
 				}
+
 				i++;
 			}
 
@@ -89,24 +81,17 @@ void jogo_base (int modo) {
 				nivel++;
 			}
 			sequencia++;
-			if (modo == 2) {
-				turno = false;
-			}
 		} else {
 			i=0;
 			while ((erro == false)&&(i < sequencia)){
-				printf("JOGADOR 2\nN%cvel %d\n", 161, nivel);
-
-				scanf("%hu", &resposta[i]);
-				setbuf(stdin, NULL);
-
-				system("cls");
+				entrada(nivel, resposta, i, turno);
 
 				if (proposta[i] != resposta[i]) {
-					printf("RESPOSTA INCORRETA\n");
-					printf("Jogador 1 venceu\n");
-					erro = true;
+						printf("RESPOSTA INCORRETA\n");
+						printf("Jogador 1 venceu\n");
+						erro = true;
 				}
+
 				i++;
 			}
 
@@ -114,7 +99,25 @@ void jogo_base (int modo) {
 				nivel++;
 			}
 			sequencia++;
-			turno = false;
+		}
+		if (modo == 2) {
+			if (turno == true) {
+				turno = false;
+			} else {
+				turno = true;
+			}
 		}
 	}
+}
+void entrada(unsigned short int nivel, unsigned short int resposta[], unsigned short int i, bool turno) {
+	if (turno == true) {
+		printf("JOGADOR 1\nN%cvel %d\n", 161, nivel);
+	} else {
+		printf("JOGADOR 2\nN%cvel %d\n", 161, nivel);
+	}
+
+	scanf("%hu", &resposta[i]);
+	setbuf(stdin, NULL);
+
+	system("cls");
 }
